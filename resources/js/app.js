0e73f1279c28e6ceb858eabfb7d34363d0aa5db8
Clive Jeffery <clive@rosedene.com>
2023-11-22 21:27:41 +0000

    Add Bulma
    Adds Bulma via npm and vite.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,1 @@
-import './bootstrap';
+// import './bootstrap'; // removed axios until needed
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,25 +10,14 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=ABeeZee:400,400&display=swap" rel="stylesheet" />
 
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
-    <body class="">
-        <div class="">
-            @if (Route::has('login'))
-                <div class="">
-                    @auth
-                        <a href="{{ url('/home') }}" class="">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">The App Here</div>
-
-        </div>
+    <body>
+        <section class="section hero">
+            <p class="title">The App</p>
+            <p class="subtitle">Here it is!</p>
+            <p>We hope you find it useful</p>
+        </section>
     </body>
 </html>
